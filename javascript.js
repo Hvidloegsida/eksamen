609633222ae10f02c8caced9202a1f4db58caa27
Hvidloegsida <idmi0001@stud.kea.dk>
2023-06-08 14:03:55 +0200

    navbar web
    navbar web er færdig om implementeret på alle sidderne med javascript

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 (function () {
-  var navButton = document.querySelector(".nav-button");
-  var dropDown = document.querySelector(".active.drop-down");
+  var navButton = document.querySelector(".nav-button1");
+  var dropDown = document.querySelector(".active.drop-down1");
 
   // Hide the border initially
   dropDown.classList.add("closed");
@@ -17,6 +17,115 @@
   dropDown.addEventListener("mouseleave", function () {
     // Add the "closed" class to hide the menu
     dropDown.classList.add("closed");
+    dropDown.classList.remove("border");
+  });
+})();
+
+(function () {
+  var navButton = document.querySelector(".nav-button2");
+  var dropDown = document.querySelector(".active.drop-down2");
+
+  // Hide the border initially
+  dropDown.classList.add("closed");
+  dropDown.classList.remove("border");
+
+  // Bind mouseenter event to the drop-down navigation button
+  navButton.addEventListener("mouseenter", function () {
+    // Remove the "closed" class to show the menu
+    dropDown.classList.remove("closed");
+    dropDown.classList.add("border");
+  });
+
+  // Bind mouseleave event to the drop-down navigation button
+  dropDown.addEventListener("mouseleave", function () {
+    // Add the "closed" class to hide the menu
+    dropDown.classList.add("closed");
+    dropDown.classList.remove("border");
+  });
+})();
+
+(function () {
+  var navButton = document.querySelector(".nav-button3");
+  var dropDown = document.querySelector(".active.drop-down3");
+
+  // Hide the border initially
+  dropDown.classList.add("closed");
+  dropDown.classList.remove("border");
+
+  // Bind mouseenter event to the drop-down navigation button
+  navButton.addEventListener("mouseenter", function () {
+    // Remove the "closed" class to show the menu
+    dropDown.classList.remove("closed");
+    dropDown.classList.add("border");
+  });
+
+  // Bind mouseleave event to the drop-down navigation button
+  dropDown.addEventListener("mouseleave", function () {
+    // Add the "closed" class to hide the menu
+    dropDown.classList.add("closed");
+    dropDown.classList.remove("border");
+  });
+})();
+
+(function () {
+  var navButton = document.querySelector(".nav-button5");
+  var dropDown = document.querySelector(".active.drop-down5");
+
+  // Hide the border initially
+  dropDown.classList.add("closed");
+  dropDown.classList.remove("border");
+
+  // Bind mouseenter event to the drop-down navigation button
+  navButton.addEventListener("mouseenter", function () {
+    // Remove the "closed" class to show the menu
+    dropDown.classList.remove("closed");
+    dropDown.classList.add("border");
+  });
+
+  // Bind mouseleave event to the drop-down navigation button
+  dropDown.addEventListener("mouseleave", function () {
+    // Add the "closed" class to hide the menu
+    dropDown.classList.add("closed");
+    dropDown.classList.remove("border");
+  });
+})();
+
+(function () {
+  var navButton = document.querySelector(".nav-button4");
+  var dropDown = document.querySelector(".active.drop-down4");
+
+  dropDown.classList.remove("border");
+
+  // Bind mouseenter event to the drop-down navigation button
+  navButton.addEventListener("mouseenter", function () {
+    // Remove the "closed" class to show the menu
+
+    dropDown.classList.add("border");
+  });
+
+  // Bind mouseleave event to the drop-down navigation button
+  dropDown.addEventListener("mouseleave", function () {
+    // Add the "closed" class to hide the menu
+    dropDown.classList.remove("border");
+  });
+})();
+
+(function () {
+  var navButton = document.querySelector(".nav-button6");
+  var dropDown = document.querySelector(".active.drop-down6");
+
+  dropDown.classList.remove("border");
+
+  // Bind mouseenter event to the drop-down navigation button
+  navButton.addEventListener("mouseenter", function () {
+    // Remove the "closed" class to show the menu
+
+    dropDown.classList.add("border");
+  });
+
+  // Bind mouseleave event to the drop-down navigation button
+  dropDown.addEventListener("mouseleave", function () {
+    // Add the "closed" class to hide the menu
     dropDown.classList.remove("border");
   });
 })();
